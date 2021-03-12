4cff4b2c15afbcc1f12937b76a459a73b1668896
Stefan Toma <34220105+Stefan9283@users.noreply.github.com>
2021-03-13 01:05:04 +0200

    getPosition Bishop + Rook FIXED

--- Source/ChessPiece.cpp
+++ Source/ChessPiece.cpp
@@ -48,178 +48,6 @@ std::vector<PieceMove> King::getPositions(Table* t) {
     return moves;
 } // {DONE}
 
-<<<<<<< Updated upstream
-    Queen::Queen(char color, vec2<int> pos, int index) : ChessPiece(color, pos, index) { score = 9; abbreviation = 'Q'; }
-    std::vector<PieceMove> Queen::getPositions(Table* t) {
-        std::vector<PieceMove> moves, rmoves, bmoves;
-
-        Rook r(color, pos, index);;
-        rmoves = r.getPositions(t);
-        moves.insert(moves.begin(), rmoves.begin(), rmoves.end());
-
-        Bishop b(color, pos, index);;
-        bmoves = b.getPositions(t);
-        moves.insert(moves.begin(), bmoves.begin(), bmoves.end());
-
-        return moves;
-    } // {DONE}
-
-    Rook::Rook(char color, vec2<int> pos, int index) : ChessPiece(color, pos, index) { score = 5; abbreviation = 'R'; }
-    std::vector<PieceMove> Rook::getPositions(Table* t) {
-        std::vector<PieceMove> moves;
-        int i;
-
-        i = 1;
-        while (i + pos.x < t->height) {
-            Square* squarePiece = t->squares[pos.x + i][i + pos.y];
-            if (t->canIPlaceItHere(this, squarePiece)) {
-                moves.push_back(PieceMove{ vec2<int>{pos.x + i, pos.y}, t->getSquareScore(squarePiece, color)});
-                if (squarePiece->piece) break;
-            } else break;
-            i++;
-        } // UP
-
-        i = -1;
-        while (i + pos.x >= 0) {
-            Square* squarePiece = t->squares[pos.x + i][pos.y];
-            if (t->canIPlaceItHere(this, squarePiece)) {
-                moves.push_back(PieceMove{ vec2<int>{pos.x + i, pos.y}, t->getSquareScore(squarePiece, color) });
-                if (squarePiece->piece) break;
-            } else break;
-            i--;
-        } // DOWN
-
-        i = 1;
-        while (pos.y + i < t->width) {
-            Square* squarePiece = t->squares[pos.x][pos.y + i];
-            if (t->canIPlaceItHere(this, squarePiece)) {
-                moves.push_back(PieceMove{ vec2<int>{pos.x, pos.y + i}, t->getSquareScore(squarePiece, color) });
-                if (squarePiece->piece) break;
-            } else break;
-            i++;
-        } // RIGHT
-
-        i = -1;
-        while (pos.y + i >= 0) {
-            Square* squarePiece = t->squares[pos.x][pos.y + i];
-            if (t->canIPlaceItHere(this, squarePiece)) {
-                moves.push_back(PieceMove{ vec2<int>{pos.x, pos.y + i}, t->getSquareScore(squarePiece, color) });
-                if (squarePiece->piece) break;
-            } else break;
-            i--;
-        } // LEFT
-
-        return moves;
-    } // {DONE}
-
-    Bishop::Bishop(char color, vec2<int> pos, int index) : ChessPiece(color, pos, index) { score = 3; abbreviation = 'B'; }
-
-    std::vector<PieceMove> Bishop::getPositions(Table* t) {
-        std::vector<PieceMove> moves;
-
-        int i;
-
-        i = 1;
-        while (t->isInside(vec2<int>{ pos.x + i, pos.y - i })) {
-            Square* squarePiece = t->squares[pos.x + i][1LL * pos.y - i];
-            if (t->canIPlaceItHere(this, squarePiece)) {
-                moves.push_back(PieceMove{ vec2<int>{pos.x + i, pos.y - i}, t->getSquareScore(squarePiece, color)});
-                if (squarePiece->piece) break;
-            }
-            i++;
-        } // LEFT UP
-
-        i = 1;
-        while(t->isInside(vec2<int>{pos.x - i, pos.y - i})) {
-            Square* squarePiece = t->squares[pos.x - i][1LL * pos.y - i];
-            if (t->canIPlaceItHere(this, squarePiece)) {
-                moves.push_back(PieceMove{ vec2<int>{pos.x - i, pos.y - i}, t->getSquareScore(squarePiece, color)});
-                if (squarePiece->piece) break;
-            }
-            i++;
-        } // LEFT DOWN
-
-        i = 1;
-        while(t->isInside(vec2<int>{pos.x + i, pos.y + i})) {
-            Square* squarePiece = t->squares[pos.x + i][1LL * i + pos.y];
-            if (t->canIPlaceItHere(this, squarePiece)) {
-                moves.push_back(PieceMove{vec2<int>{pos.x + i, i + pos.y}, t->getSquareScore(squarePiece, color)});
-                if (squarePiece->piece) break;
-            }
-            i++;
-        } // RIGHT UP
-
-        i = 1;
-        while(t->isInside(vec2<int>{pos.x - i, pos.y + i})) {
-            Square* squarePiece = t->squares[pos.x - i][1LL * pos.y + i];
-            if (t->canIPlaceItHere(this, squarePiece)) {
-                moves.push_back(PieceMove{vec2<int>{pos.x - i, pos.y + i}, t->getSquareScore(squarePiece, color)});
-                if (squarePiece->piece) break;
-            }
-            i++;
-        } // RIGHT DOWN
-
-        return moves;
-    } // {DONE}
-
-    Knight::Knight(char color, vec2<int> pos, int index) : ChessPiece(color, pos, index) { score = 3; abbreviation = 'N'; }
-    std::vector<PieceMove> Knight::getPositions(Table* t) {
-        std::vector<PieceMove> moves;
-
-        vec2<int> v{};
-
-        // UP
-        v = vec2<int>{ pos.x + 1, pos.y + 2 };
-        if (t->isInside(v) && t->canIPlaceItHere(this, t->squares[v.x][v.y]))
-            moves.push_back(PieceMove{ v, t->getSquareScore(t->squares[v.x][v.y], color) });
-        v = vec2<int>{ pos.x - 1, pos.y + 2 };
-        if (t->isInside(v) && t->canIPlaceItHere(this, t->squares[v.x][v.y]))
-            moves.push_back(PieceMove{ v, t->getSquareScore(t->squares[v.x][v.y], color) });
-
-        // DOWN
-        v = vec2<int>{ pos.x + 1, pos.y - 2 };
-        if (t->isInside(v) && t->canIPlaceItHere(this, t->squares[v.x][v.y]))
-            moves.push_back(PieceMove{ v, t->getSquareScore(t->squares[v.x][v.y], color) });
-        v = vec2<int>{ pos.x - 1, pos.y - 2 };
-        if (t->isInside(v) && t->canIPlaceItHere(this, t->squares[v.x][v.y]))
-            moves.push_back(PieceMove{ v, t->getSquareScore(t->squares[v.x][v.y], color) });
-
-        // RIGHT
-        v = vec2<int>{ pos.x + 2, pos.y + 1 };
-        if (t->isInside(v) && t->canIPlaceItHere(this, t->squares[v.x][v.y]))
-            moves.push_back(PieceMove{ v, t->getSquareScore(t->squares[v.x][v.y], color) });
-        v = vec2<int>{ pos.x + 2, pos.y - 1 };
-        if (t->isInside(v) && t->canIPlaceItHere(this, t->squares[v.x][v.y]))
-            moves.push_back(PieceMove{ v, t->getSquareScore(t->squares[v.x][v.y], color) });
-
-        // LEFT
-        v = vec2<int>{ pos.x - 2, pos.y + 1 };
-        if (t->isInside(v) && t->canIPlaceItHere(this, t->squares[v.x][v.y]))
-            moves.push_back(PieceMove{ v, t->getSquareScore(t->squares[v.x][v.y], color) });
-        v = vec2<int>{ pos.x - 2, pos.y - 1 };
-        if (t->isInside(v) && t->canIPlaceItHere(this, t->squares[v.x][v.y]))
-            moves.push_back(PieceMove{ v, t->getSquareScore(t->squares[v.x][v.y], color) });
-
-        return moves;
-    } // {DONE}
-
-    Pawn::Pawn(char color, vec2<int> pos, int index) : ChessPiece(color, pos, index) { score = 1; abbreviation = 'P';}
-    std::vector<PieceMove> Pawn::getPositions(Table* t) {
-        std::vector<PieceMove> moves;
-
-        int direction = color == 'w' ? 1 : -1;
-
-        // 2 UP
-        if (!wasMoved && t->isInside(vec2<int>{pos.x + 2 * direction, pos.y}) && !t->squares[pos.x + 2 * direction][pos.y]->piece)
-            moves.push_back(PieceMove{vec2<int>{pos.x + 2 * direction, pos.y}});
-
-        // 1 UP
-        if (t->isInside(vec2<int>{pos.x + 1 * direction, pos.y}) && !t->squares[pos.x + 1 * direction][pos.y]->piece)
-            moves.push_back(PieceMove{ vec2<int>{pos.x + 1 * direction, pos.y} });
-
-        // LEFT
-        if (t->isInside(vec2<int>{pos.x + 1 * direction , pos.y - 1 * direction})
-=======
 Queen::Queen(char color, vec2<int> pos, int index) : ChessPiece(color, pos, index) { score = 9; abbreviation = 'Q'; }
 std::vector<PieceMove> Queen::getPositions(Table* t) {
     std::vector<PieceMove> moves, rmoves, bmoves;
@@ -240,7 +68,6 @@ std::vector<PieceMove> Rook::getPositions(Table* t) {
     std::vector<PieceMove> moves;
     int i;
 
-    std::cout << "sup\n";
     i = 1;
     while (i + pos.x < t->height) {
         Square* squarePiece = t->squares[pos.x + i][pos.y];
@@ -250,7 +77,6 @@ std::vector<PieceMove> Rook::getPositions(Table* t) {
         if (squarePiece->piece) break;
         i++;
     } // UP
-    std::cout << "sup\n";
     i = -1;
     while (i + pos.x >= 0) {
         Square* squarePiece = t->squares[pos.x + i][pos.y];
@@ -261,7 +87,6 @@ std::vector<PieceMove> Rook::getPositions(Table* t) {
 
         i--;
     } // DOWN
-    std::cout << "sup\n";
     i = 1;
     while (pos.y + i < t->width) {
         Square* squarePiece = t->squares[pos.x][pos.y + i];
@@ -271,7 +96,6 @@ std::vector<PieceMove> Rook::getPositions(Table* t) {
         if (squarePiece->piece) break;
         i++;
     } // RIGHT
-    std::cout << "sup\n";
     i = -1;
     while (pos.y + i >= 0) {
         Square* squarePiece = t->squares[pos.x][pos.y + i];
@@ -281,7 +105,6 @@ std::vector<PieceMove> Rook::getPositions(Table* t) {
         if (squarePiece->piece) break;
         i--;
     } // LEFT
-    std::cout << "sup\n";
     return moves;
 } // {DONE}
 
@@ -402,7 +225,6 @@ std::vector<PieceMove> Pawn::getPositions(Table* t) {
 
     // LEFT
     if (t->isInside(vec2<int>{pos.x + 1 * direction , pos.y - 1 * direction})
->>>>>>> Stashed changes
             && t->squares[pos.x + 1 * direction][pos.y - 1 * direction]->piece && t->squares[pos.x + 1 * direction][pos.y - 1 * direction]->piece->color != color)
             moves.push_back(PieceMove{ vec2<int>{pos.x + 1 * direction, pos.y - 1 * direction}, t->getSquareScore(t->squares[pos.x + 1 * direction][pos.y - 1 * direction], color)});
 
