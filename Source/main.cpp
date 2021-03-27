daaaaa76e7c57f7ee4eb0e2487325751ad1712f5
iDanyel <ovidiudaniel99@gmail.com>
2021-03-27 20:06:48 +0200

    Updated

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,20 +2,6 @@
 
 int main() {
     Engine e;
-
-    /*Table* table = new Table();
-
-    table->moveInAdvance("(e2, e4), (b8, c6), (b1, c3), (g8, f6), (d2, d4), (c6, b8), (d4, d5), (d7, d6), (e4, e5), (d6, e5), (d1, g4), (c8, g4), (c1, g5), (d8, d6), (g5, f6), (e7, f6), (f1, a6), (b8, a6), (c3, b5), (d6, d7), (b5, c7), (d7, c7), (g1, f3), (c7, b8), (f3, g5), (e8, e7), (g5, h7), (h8, h7), (h2, h3), (b8, d6), (c2, c4), (d6, d7), (d5, d6), (e7, d6), (h3, g4)", 'w');
-
-    table->printGameBoard();
-
-    std::cout << table->getBestMove(5);*/
-
-    /*table->moveInAdvance("(g1, f3), (g8, f6), (e2, e3), (g7, g6), (f1, b5), (f8, h6), (d2, d4), (h6, f8), (b2, b3), (f6, g8), (b1, c3), (e7, e6), (e1, g1), (b8, c6), (e3, e4), (h7, h6), (c1, f4), (h6, h5), (e4, e5), (f8, g7), (f4, g3), (h5, h4), (g3, h4), (g8, e7), (c3, e2), (e8, f8), (a2, a3), (c6, a5), (d1, d2), (c7, c6), (b5, d3), (b7, b6), (d2, e1), (h8, h5), (h4, g5), (f8, g8), (a1, c1), (h5, h8), (h2, h3), (d8, e8), (e2, f4), (e8, f8), (e1, b4), (c6, c5), (d4, c5), (a5, b7), (c5, b6), (a7, a6), (d3, e4), (e7, c6), (b4, c3), (c6, d8), (e4, g6), (f7, g6), (f4, g6), (f8, a3), (c1, a1), (a3, d6), (g5, d8), (b7, d8), (g6, h8), (d6, b6), (h8, g6), (d8, c6), (c3, d2), (a8, a7), (f1, b1), (b6, b8), (d2, c3), (b8, b6), (b1, d1), (d7, d5)", 'w');
-    table->printGameBoard();
-
-    std::cout << table->getBestMove(5);*/
-
     return 0;
 }
 
